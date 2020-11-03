18765d6663ce5ba7896cfe1858fb9900700d5e57
ZitaRR <Haeggqvist1998@hotmail.com>
2020-11-03 18:51:14 +0100

    Minor stuff i guess

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,48 +1,84 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+    [SerializeField]
+    private int moves;
+    [SerializeField]
+    private new Camera camera;
+    [SerializeField]
+    private Vector3 cameraOffset;
+
     private Vector3 start;
-    private Vector3 end;
+    private List<Node> availableNodes;
     private Stack<Node> path;
+
+    private LevelManager level;
+
+    private void Awake()
+    {
+        level = GameObject.FindGameObjectWithTag("GameManager")
+            .GetComponent<LevelManager>();
+        GetComponent<MeshRenderer>().material.color = Color.grey;
+    }
 
     private void Start()
     {
-        GetComponent<MeshRenderer>().material.color = Color.white;
-        do
+        var node = level.GetRandomWalkableNode();
+        transform.position = node.GetVector();
+    
+        availableNodes = level.Pathfinding.GetWalkableNodes(node, moves);
+        foreach (var n in availableNodes)
         {
-            int x = Random.Range(0, WorldGeneration.Size);
-            int z = Random.Range(0, WorldGeneration.Size);
-            start = new Vector3(x, .5f, z);
-        } while (!WorldGeneration.GetNode(start).Walkable);
+            n.ChangeColour(Color.green);
+        }
 
-        transform.position = start;
+        camera.transform.position = transform.position + cameraOffset;
     }
-
+    
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+        
             if (!Physics.Raycast(ray, out hit))
                 return;
 
-            path = ASTAR.FindPath(transform.position, hit.point);
+            var node = level.GetNodeAtPosition(hit.collider.gameObject.transform.position);
+            if (!availableNodes.Contains(node))
+                return;
+    
+            foreach (var n in availableNodes)
+            {
+                n.ChangeColour();
+            }
+            path = level.Pathfinding.FindPath(transform.position, node.GetVector());
         }
-
+        
         if (path == null || path.Count <= 0)
             return;
-
+        
         Movement(path.Peek().GetVector());
-
+        
         if (path.Peek().GetVector() == transform.position)
-            path.Pop();
+        {
+            var current = path.Pop();
+            if (path.Count <= 0)
+            {
+                availableNodes = level.Pathfinding.GetWalkableNodes(current, moves);
+                foreach (var node in availableNodes)
+                {
+                    node.ChangeColour(Color.green);
+                }
+            }
+        }
     }
-
+    
     protected void Movement(Vector3 destination)
     {
         var movement = Vector3.MoveTowards(transform.position, destination, 2f * Time.deltaTime);
@@ -50,14 +86,6 @@ public class Entity : MonoBehaviour
         var rotation = Quaternion.LookRotation(direction);
         transform.position = movement;
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10f * Time.deltaTime);
-    }
-    private void NewDestination()
-    {
-        do
-        {
-            int x = Random.Range(0, WorldGeneration.Size);
-            int z = Random.Range(0, WorldGeneration.Size);
-            end = new Vector3(x, .5f, z);
-        } while (!WorldGeneration.GetNode(end).Walkable);
+        camera.transform.position = movement + cameraOffset;
     }
 }
